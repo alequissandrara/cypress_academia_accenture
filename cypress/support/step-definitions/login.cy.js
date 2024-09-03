7ba69b5af5f67d5cc1ba52645df66ef09d9f1603
alequissandrara <alequissandrarayane123@gmail.com>
2024-09-03 20:43:15 -0300

    Automação de Login

--- cypress/support/step-definitions/login.cy.js
+++ cypress/support/step-definitions/login.cy.js
@@ -1,7 +1,12 @@
+import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
+
 Given("que o acesso o portal My Shop", () => {
   cy.visit("/");
 });
 
-When("realizo login no portal", () => {});
+When("realizo login no portal", () => {
+  cy.acessarLogin();
+  cy.realizarLogin();
+});
 
 Then("valido o acesso a home", () => {});
