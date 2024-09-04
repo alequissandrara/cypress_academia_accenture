118ae9ca54d1fdc8cd3461b41e634c359375c016
alequissandrara <alequissandrarayane123@gmail.com>
2024-09-04 20:18:33 -0300

    Validações de Produto e acesso a categoria Women.

--- cypress/support/step-definitions/validacoes-produto.cy.js
+++ cypress/support/step-definitions/validacoes-produto.cy.js
@@ -1,16 +1,16 @@
 import { And, Then } from "cypress-cucumber-preprocessor/steps";
 
-And("acessar a categoria women", () => {
-  cy.acessarCetegoriaWoman();
-});
-
-And("acesso o produto {string}", (produto) => {
-  cypress.env("produto", produto);
-  cy.clicarProduto(produto);
+And("acesso a categoria Women", () => {
+  cy.get('a[title="Women"]').click();
 });
 
 And("valido a quantidade de produtos em tela", () => {
   cy.validarQtdProdutos();
+});
+
+And("acesso o produto {string}", (produto) => {
+  Cypress.env("produto", produto);
+  cy.clicarProduto(produto);
 });
 
 Then("realizo as validações do produto", () => {
